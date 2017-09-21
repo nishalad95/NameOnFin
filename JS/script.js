88ce65f8cd699fa01e52778a0020be598b9a24a5
nishalad95 <nislad@NISLAD-GB.uk.oracle.com>
2017-09-21 15:54:11 +0100

    added panning of background fin image

--- JS/script.js
+++ JS/script.js
@@ -12,38 +12,22 @@ $(document).ready(function(){
                 var name_array = obj.NAME;
 
                 for(var i = 0; i < name_array.length; i++){
-                    $(".name_area").append("<div id='name_'>" + name_array[i] + ", &nbsp</div>");
+                $(".namesHolder").append("<div class='name' id='name"+i+"'>" + name_array[i] + ", &nbsp</div>");
                 }
             }
         });
     }
 
 getAllNames();
-    $("#search").submit(function(){
-        var query = $("#search").serialize();
-        query = query.split('=');
-        $.ajax({
-            type: "GET",
-            dataType: "json",
-            url: "http://129.146.81.161/fin/?func_name=search&q=" + query[1],
-
-            success: function(data){
-                    var names = JSON.stringify(data);
-                    var obj = $.parseJSON(names);
-                    var name_array = obj.NAME;
-                    for(var i = 0; i < name_array.length; i++){
-                            $(".name_area").append("<div id='name_' style='z-index: 17;'>" + name_array[i] + "</div>");
-                    }
-            }
-        });	
-    });
     
     
     /* This is for the search bar, selecting only the names that matter. */
-	$("#search_names").submit(function(){
+	$("#search_names").submit(function(e){
 		
-                $("#overlay").style.display = "block";
-                $("#greeting").style.display = "none";
+                e.preventDefault();
+                $("#overlay").hide();
+                $("#greeting").hide();
+                $(".namesScrollBar").remove();
                 
 		var data = $("#search_names").serializeArray().reduce(function(obj, item){
 		
@@ -63,28 +47,62 @@ getAllNames();
 		// This is the search results.. Here is the best bet for generating the panel.
 		searchResults = names.NAME;
                 
-                alert(searchResults);
-                
                 if (searchResults.length > 1) {
-                    $("#namesScrollBar").style.display = "block";
+                    $(".name_area").append("<div class='namesScrollBar' id='scrollBarStyle'></div>");
+                    $(".namesScrollBar").append("<div class='searchQuery'>Results for: " + searchTerm +"</div>");
+                    $(".namesScrollBar").append("<ul class='searchList'>");
+                    for (var i = 0; i < searchResults.length - 1; i++) {
+                        $(".searchList").append("<a id='nameLink' href='#'><li class='listBorder'>" + searchResults[i] + "</li></a>");
+                    }    
                 } else if (searchResults.length === 1) {
                     //go straight to the name in the canvas
                 } else {
                     alert("No results found");
                 }
-		
+                
+                
+                /* finIMG panning */
+                var img_ele = null, x_cursor = 0, y_cursor = 0, x_img_ele = 0, y_img_ele = 0;
+                
+                function start_drag() {
+                    img_ele = this;
+                    console.log('inside start_drag var img_ele set to : ' + this);
+                    x_img_ele = window.event.clientX - document.getElementById('finIMG').offsetLeft;
+                    y_img_ele = window.event.clientY - document.getElementById('finIMG').offsetTop;
+                    console.log('start_drag : ' + 'x_img_ele:', x_img_ele, 'y_img_ele:', y_img_ele);
+                }
+
+                function stop_drag() {
+                    console.log('stop drag');
+                    img_ele = null;
+                }
+
+                function while_drag() {
+                    console.log('while_drag: ', window.event);
+                    var x_cursor = window.event.clientX;
+                    var y_cursor = window.event.clientY;
+                    if (img_ele !== null) {
+                        img_ele.style.left = (x_cursor - x_img_ele) + 'px';
+                        img_ele.style.top = ( window.event.clientY - y_img_ele) + 'px';
+    
+                        console.log('while_drag: ','x_cursor', x_cursor, 'x_img_ele', x_img_ele, 'y_cursor', y_cursor, 'y_img_ele', y_img_ele,'Image left and top', img_ele.style.left+' - '+img_ele.style.top);
+                    }
+                }
+
+                $("#finIMG").on('mousedown', start_drag);
+                $(".FinSVG").on('mousemove', while_drag);
+                $(".FinSVG").on('mouseup', stop_drag);
+                
 	
 	});
     
- 
-        
-    
-    
+                
+                
     function loadImages() {
-        const TOTALNUMSELFIES = 70;
+        const TOTALNUMSELFIES = 68;
        
         for (var i = 1; i <= TOTALNUMSELFIES; i++) {
-            var thumbnailContainer = "<a href=\"#img" + i +  "\"><img id='selfie' src='images/Selfies/" + i + ".jpg' alt='selfie' /></a>";
+            var thumbnailContainer = "<a href=\"#img" + i +  "\"><img id='selfie' src='images/Selfies/" + i + ".png' alt='selfie' /></a>";
             
             var prev = i - 1;
             var next = (i + 1) % 70;
@@ -99,7 +117,7 @@ getAllNames();
             /*previous button*/
             $(".lightbox#img" + i + "").append("<a href=\"#img" + prev + "\" class='previous'>&lt;</a>");
             /* lightbox image */
-            $(".lightbox#img" + i + "").append("<a href=\"#_\"><img src=\"images/Selfies/" + i + ".jpg\" alt=\"selfie\" /></a>");
+            $(".lightbox#img" + i + "").append("<a href=\"#_\"><img src=\"images/Selfies/" + i + ".png\" alt=\"selfie\" /></a>");
             /* exit button */
             $(".lightbox#img" + i + "").append("<a href=\"#_\" class='exit'>&times;</a>");
             /* next button*/
