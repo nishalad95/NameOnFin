03626106788cc9d0c7d5d495ab0cc40465448881
nishalad95 <nislad@NISLAD-GB.uk.oracle.com>
2017-09-23 21:12:38 +0100

    initial name translation to center of box, after search, implemented WIP

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,6 @@
 // preload all names on fin to view when browser loads
 $(document).ready(function(){
+    var namesIDMap = {};
     function getAllNames(){
         $.ajax({
             type: "GET",
@@ -10,9 +11,11 @@ $(document).ready(function(){
                 var names = JSON.stringify(data);
                 var obj = $.parseJSON(names);
                 var name_array = obj.NAME;
-
+           
                 for(var i = 0; i < name_array.length; i++){
-                $(".finNames").append("<div class='name' id='"+name_array[i]+"'>" + name_array[i] + ", &nbsp</div>");
+                    $(".finNames").append("<div class='name' id='name"+i+"'>" + name_array[i] + ", &nbsp</div>");
+                    // add to namesIDMap
+                    namesIDMap[name_array[i]] =  i;
                 }
             }
         });
@@ -23,50 +26,67 @@ getAllNames();
 
 /** Draggable fin and names **/
 $( function() {
-    $( "#draggable" ).draggable();
+    $("#draggable").draggable();
 } );
 
     /* This is for the search bar, selecting only the names that matter. */
 	$("#search_names").submit(function(e){
 		
                 e.preventDefault();
-                $(".namesHolder").css({transform: 'scale(1)'});
                 $("#overlay").hide();
                 $("#greeting").hide();
                 $(".namesScrollBar").remove();
                 
 		var data = $("#search_names").serializeArray().reduce(function(obj, item){
-		
 			obj[item.name] = item.value;
 			return obj;
 		}, {});
 		
 		var searchTerm = data["name"];
-		
 		var names = $.parseJSON($.ajax({
-		
 			url: "http://129.146.81.161/fin/?func_name=search&q=" + searchTerm,
 			dataType: "JSON",
 			async: false
 		}).responseText);
 
 		searchResults = names.NAME;
+                $(".namesHolder").css({transform: 'scale(1)'});
+                var x, y, currentID;
+                
                 
                 if (searchResults.length > 1) {
-                    $(".name_area").append("<div class='namesScrollBar' id='scrollBarStyle'></div>");
+                    $(".name_area").append("<div class='namesScrollBar' id='panel'></div>");
                     $(".namesScrollBar").append("<div class='searchQuery'>Results for: " + searchTerm +"</div>");
                     $(".namesScrollBar").append("<ul class='searchList'>");
+                    
                     for (var i = 0; i < searchResults.length - 1; i++) {
                         $(".searchList").append("<a id='nameLink' href='#'><li class='listBorder"+ i +"'>" + searchResults[i] + "</li></a>");
                     }
                     
                     $('li[class^="listBorder"]').on('click', function(){
-                        selectedName = $(this).text();
-                        $(".namesHolder").css({transform: 'scale(10)'});
+                        var selectedName = $(this).text();
+                        currentID = namesIDMap[selectedName];
+                 
+                        // calc difference between center coord (x,y) and name div (x,y)
+                        x = 403.75 - $("#name" + currentID).position().left;
+                        y = 250 - $("#name" + currentID).position().top;
+                        console.log("x is: ", x);
+                        console.log("y is: ", y);
+                        
+                        // move the name to center
+                        //$(".namesHolder").css({transform: 'scale(10)'});
+                        $(".namesHolder").css({transform: 'translate('+x+'px,'+y+'px)'});
                     });
-                
+                    
+                    
                 } else if (searchResults.length === 1) {
-                    //go straight to the name in the canvas
+                    //go straight to the name
+                    currentID = namesIDMap[searchResults];
+                    x = 403.75  - $("#name" + currentID).position().left;
+                    y = 250 - $("#name" + currentID).position().top;
+                    $(".namesHolder").css({transform: 'translate('+x+'px,'+y+'px)'});
+                  
+                    
                 } else {
                     alert("No results found");
                 }
