aeaae32dc54f70800339a4b3a2b126da344c2217
nishalad95 <nishalad95@gmail.com>
2017-11-07 16:33:41 +0000

    changes to names panel limit and final checks

--- PHP/search.php
+++ PHP/search.php
@@ -17,13 +17,13 @@ if(mysqli_connect_errno()){
 	echo "Error code: " . mysqli_connect_error();
 }
 
-$sql = "(SELECT `name`, `id`, `key_` FROM `schools` WHERE `name` LIKE '%".$term."%' OR `email` LIKE '%".$term."%' LIMIT 25) 
+$sql = "(SELECT `name`, `id`, `key_` FROM `schools` WHERE `name` LIKE '%".$term."%' OR `email` LIKE '%".$term."%' LIMIT 20) 
 		UNION 
-		(SELECT `name`, `id`, `key_` FROM `fin_names_near` WHERE `name` LIKE '%".$term."%' OR `email` LIKE '%".$term."%'  LIMIT 25)
+		(SELECT `name`, `id`, `key_` FROM `fin_names_near` WHERE `name` LIKE '%".$term."%' OR `email` LIKE '%".$term."%'  LIMIT 80)
 		UNION
-		(SELECT `name`, `id`, `key_` FROM `fin_names_off` WHERE `name` LIKE '%".$term."%' OR `email` LIKE '%".$term."%' LIMIT 25)
+		(SELECT `name`, `id`, `key_` FROM `fin_names_off` WHERE `name` LIKE '%".$term."%' OR `email` LIKE '%".$term."%' LIMIT 80)
 		UNION
-		(SELECT `name`, `id`, `key_` FROM `enhanced` WHERE `name` LIKE '%".$term."%' OR `email` LIKE '%".$term."%' LIMIT 25)";
+		(SELECT `name`, `id`, `key_` FROM `enhanced` WHERE `name` LIKE '%".$term."%' OR `email` LIKE '%".$term."%' LIMIT 20)";
 
 $data = array();
 
